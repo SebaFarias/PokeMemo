b5957ad04aa449af2b6d0dc45be22f9d4f08f382
sebafarias <sebastian.fariasb@usach.cl>
2023-09-07 10:44:08 -0300

    bugfix /playing - debug route not rendering

--- src/app/playing/page.tsx
+++ src/app/playing/page.tsx
@@ -1,10 +1,10 @@
-import { usePokeSessionContext } from "~/hooks/usePokeContext"
+
 
 const Playing = () => {
-  const [context, setContext] = usePokeSessionContext()
   return(
     <>
-      {JSON.stringify(context)}
+      hi
     </>
   )
 }
+export default Playing
