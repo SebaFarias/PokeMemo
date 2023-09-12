022fc73f3d367a034dd55cdfefda8ee6bc8cc0cb
sebafarias <sebastian.fariasb@usach.cl>
2023-09-12 18:26:29 -0300

    model working ok

--- src/model/boardMatrix/IdMatrix.ts
+++ src/model/boardMatrix/IdMatrix.ts
@@ -2,12 +2,24 @@ import { Position } from "./PokeMemoMatrix"
 
 const MIN_POKE_RANGE = 1
 const MAX_POKE_RANGE = 649
+export enum CardState {
+  SOLVED = 'solved',
+  HIDDEN = 'hidden',
+  SHOWN = 'shown',
+}
+export interface Card {
+  id: number,
+  status: CardState,
+}
+
+
 export class IdMatrix {
   private idRange: [number,number]
   private rows: number
   private columns: number
   private idList: Set<number>
   private matrix: number[][]
+  private displayData: Card[][]
 
   constructor( rows:number , columns:number){
     this.idRange = [MIN_POKE_RANGE,MAX_POKE_RANGE]
@@ -15,12 +27,13 @@ export class IdMatrix {
     this.columns = columns
     this.idList = this.generateIdsSet(this.idRange)
     this.matrix = this.generateIdMatrix()
+    this.displayData = this.generatePokeMatrix()
   }
   
   generateIdsSet(range:[number,number]): Set<number>{
     const [min,max] = range
     const idsSet = new Set<number>()
-    while(idsSet.size<Math.floor(this.rows*this.columns/2)){
+    while( idsSet.size < Math.floor( this.rows*this.columns/2 )){
       idsSet.add(Math.floor(Math.random() * (max - min + 1)) + min)
     }
     return idsSet
@@ -38,23 +51,25 @@ export class IdMatrix {
     return positions
   }
   getAvailableSpaces(){
-    return this.matrix.map((row,rowIndx)=>row.map((id,colIndx)=>{
-      if(typeof id === 'undefined'){
-        const position: Position<number,number> = [rowIndx,colIndx]
+    const thisMatrix = this.matrix
+    return this.getAllPositions().map( position => {
+      const [row,col] = position
+      if(typeof thisMatrix[row]![col] === 'undefined'){
         return position
       }
-    })).flat()
+    })
   }
   getRandomPosition(){
     let position : Position<number,number> = [-1,-1]
     const availables = this.getAvailableSpaces()
     if(availables.length>0){
       const randomIndex = Math.floor(Math.random() * availables.length)
-      position = availables[randomIndex]!
+      if(availables[randomIndex])position = availables[randomIndex]!
     }
     return position 
   }
   getMatrix = () => this.matrix
+  getDisplayMatrix = () => this.displayData
   
   reset(rows=0,cols=0){
     if( rows > 0 ) this.rows = rows
@@ -64,19 +79,37 @@ export class IdMatrix {
     if(this.rows*this.columns%2!==0)
     this.matrix[this.rows-1]=[...Array(this.columns-1)]
   }
+  getDistribution(){
+    const positions = this.getAllPositions()
+    const distribution: {[pos:string]: number} = Object.fromEntries(this.getAllPositions().map(value=>[value,-1]))
+    this.idList.forEach( id => {
+      const randomIndex1 = Math.floor( Math.random() * positions.length )
+      const [rowPos1,colPos1] = positions.splice(randomIndex1,1)[0]!
+      const randomIndex2 = Math.floor( Math.random() * positions.length )
+      const [rowPos2,colPos2] = positions.splice(randomIndex2,1)[0]!
+      distribution[`${rowPos1},${colPos1}`] = id
+      distribution[`${rowPos2},${colPos2}`] = id
+    })
+    return distribution
+  }
   generateIdMatrix( rows = 0, cols = 0){
     this.reset(rows,cols)
-    this.idList.forEach( (id) => {
-      console.log(id)
-      const [rowPos1,colPos1] = this.getRandomPosition()
-      const row1 = this.matrix[rowPos1]
-      if(row1 && rowPos1> -1 && colPos1> -1) row1[colPos1] = id 
-      const [rowPos2,colPos2] = this.getRandomPosition()
-      const row2 = this.matrix[rowPos2]
-      if(row2 && rowPos2> -1 && colPos2> -1) row2[colPos2] = id      
-      console.log(this.matrix)
-    })
+    const distribution = this.getDistribution()
+    this.matrix = this.matrix
+    .map( ( row,rowIndx ) => row
+    .map( (point,colIndx) => distribution[`${rowIndx},${colIndx}`]!))
     return this.matrix
+  }
+  generatePokeMatrix(){
+    this.displayData = this.matrix
+    .map( row => row
+    .map( id => {
+      return { 
+        id:id,
+        status: CardState.HIDDEN
+      }
+    }))
+    return this.displayData
   }
 }
 
